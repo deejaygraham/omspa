0aaccb628bf19896d98ae2a5d8f3919c6eff2990
Derek Graham <deejaygraham@gmail.com>
2026-05-29 09:45:09 +0100

    Set up Eleventy configuration with Markdown support
    Configure Eleventy with MarkdownIt and directory settings.

---
+++ .eleventy.cjs
@@ -0,0 +1,18 @@
+const MarkdownIt = require("markdown-it");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setLibrary("md", new MarkdownIt({ html: true }));
+  eleventyConfig.addPassthroughCopy("img");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["md", "njk"]
+  };
+};
